d613009ce4541a5f0ef595542a33db5764574400
Derek Caelin <derekcaelin@protonmail.com>
2022-08-22 14:54:34 -0400

    testing 3d terrian in sacred sierra

--- Sacred Sierra/src/config.js
+++ Sacred Sierra/src/config.js
@@ -1,5 +1,8 @@
 var config = {
-    style: 'mapbox://styles/mapbox/satellite-v9',
+   // style: 'mapbox://styles/mapbox/satellite-v9',
+    style: 'mapbox://styles/derekcaelintm/cl750jezf000l14lozbesd9rn',
+   // style: 'mapbox://styles/mapbox-map-design/ckhqrf2tz0dt119ny6azh975y',
+   // style: 'mapbox://styles/mapbox/streets-v11',
     accessToken: 'pk.eyJ1IjoiZGVyZWtjYWVsaW50bSIsImEiOiJjbDZxd2UyOGMwYWttM2psam1vMW1wcHVvIn0.Nv5ZZQ5Fu8_lTJ2FWEXABA',
     showMarkers: false,
     markerColor: '#3FB1CE',
@@ -99,21 +102,23 @@ var config = {
             onChapterExit: []
         },
         {
-            id: 'immaculata-casimero',
+            id: 'sacred-sierra',
             alignment: 'left',
             hidden: false,
-            title: 'Immaculata Casimero',
+            title: 'Sacred Sierra',
             //image: 'images/immaculata-casimero.png',
             //caption: 'Photo Credits: "Kanuku Mountains"/"Mangroves"/"Giant Amazon Water Lily"/"Forest and Savannah Grassland" - ©Pete Oxford/iLCP; "Konashen Women" - ©Piotr Naskrecki; "Giant Otter" - ©Conservation International/photo by John Martin; "Scarlet Macaw" © Conservation International/photo by Haroldo Castro',
             description: '<p style="font-size:18px;">"Working with my people over the years has made me aware of the concerns, issues, and struggles of my people, especially as it relates to mining activities that are being carried on our traditional lands which is causing devastating impacts on the environment and contributing to climate change, social and health issues." </p> <img src="images/immaculata-casimero.png"><p> "I am Immaculata Casimero, 38 years of age, an indigenous woman, who belongs to the Wapichan nation in Aishalton (Aishara Toon) village Guyana. Presently, I am employed as the communications officer with the South Rupununi District Council (SRDC), a local Indigenous Organisation that represents 21 communities within the Wapichan territory. Besides my job; I hold the positions as chairperson of the Aishara Toon Women&apos;s Association (AWA), a community based organization which is comprised of 15 women in the Wapichan village of Aishalton; and the Aishalton Primary School feeding programme which provides hot meals on a daily basis to over 177 students.</p> </p>My journey as a village leader began in 2015 when I was elected as a village councillor where I served in the capacity as Secretary of the Aishalton Village Council for 3 years.</p> <p> In January of 2020, I helped to organize the first Rupununi Indigenous Women’s Conference which brought together over eighty women to discuss and focus on gender, leadership, environmental issues, and empowerment. I am also a co-founder of Wapichan Women’s Movement (WWM) which is the women’s arm of the South Rupununi District Council. The Wapichan Women’s Movement aims toward improving women&apos;s livelihood through collective action, entrepreneurship and empowerment."</p>',
             location: {
-                center: [-59.63122,  2.46687],
-                zoom: 8.86,
-                pitch: 0,
-                bearing: 0
+                center: [-73.65023,  10.78513],
+                zoom: 11.07,
+                pitch: 60,
+                bearing: 0,
+                use3dTerrain: true
             },
             mapAnimation: 'flyTo',
-            rotateAnimation: false,
+            
+            rotateAnimation: true,
             mapInteractive: false,
             callback: '',
             onChapterEnter: [],
