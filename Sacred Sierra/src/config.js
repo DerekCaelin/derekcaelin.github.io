792042cffed9274b5c504e119b6c07171972ee98
Derek Caelin <derekcaelin@protonmail.com>
2022-10-06 14:19:12 -0400

    more anei locations
    added several layers of polygons and lines

--- Sacred Sierra/src/config.js
+++ Sacred Sierra/src/config.js
@@ -58,6 +58,26 @@ var config = {
                 {
                     layer: 'anei-locations',
                     opacity: 0
+                 },
+                 {
+                    layer: 'ari',
+                    opacity: 0
+                 },
+                 {
+                    layer: 'anei-cienaga',
+                    opacity: 0
+                 },
+                 {
+                    layer: 'linea-negra-provectada',
+                    opacity: 0
+                 },
+                 {
+                    layer: 'parques-naturales',
+                    opacity: 0
+                 },
+                 {
+                    layer: 'rios',
+                    opacity: 0
                  }
             ],
             onChapterExit: [
@@ -116,7 +136,7 @@ var config = {
                 zoom: 9.39,
                 pitch: 58.00,
                 bearing: 0,
-                speed: 0.7
+                speed: 0.2
             },
             mapAnimation: 'flyTo',
             rotateAnimation: true,
@@ -135,6 +155,170 @@ var config = {
             ]
         },
         {
+            id: 'resguardo-indigena',
+            alignment: 'right',
+            hidden: false,
+            title: 'Resguardo Indigena',
+            //image: 'images/woman1.png',
+            //caption: 'Source image caption',
+            //website: '<a href="https://www.native-land.ca" target="_blank">Native Land website</a>',
+           // author: 'Rudo Kemper, Digital Democracy',
+            //legend: '<span style="font-size: 0.85em;"><em>Indigenous community names on the map courtesy of Native Land Digital</em></span>',
+            description: 'The Sierra Nevada de Santa Maria is home to more than 50.000 inhabitants among four indigenous communities, direct descendants of the ancient Tayrona culture. This landscapes is a place of worship - no less worthy of veneration than a temple, church, or mosque. As guardians of the earth, they believe it is their duty to protect natural resources and show an ecological alternative to industrialized societies. To the indigenous communities of this Sierra, this landscape is the literal, beating heart of the world.<br><iframe width="100%" height="315" src="https://www.youtube.com/embed/IhYYBcc2lEk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+            location: {
+                center: [-72.96832, 10.23731],
+                zoom: 8.47,
+                pitch: 45.00,
+                bearing: -11.20,
+                speed: 0.7
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            spinGlobe: false,
+            mapInteractive: false,
+            callback: '',
+            onChapterEnter: [
+                
+                    {
+                        layer: 'ari',
+                       opacity: 1,
+                       
+                     }
+                 
+            ],
+            onChapterExit: [
+                
+                    {
+                        layer: 'ari',
+                       opacity: 0
+                     }
+                
+            ]
+        },
+        {
+            id: 'rios',
+            alignment: 'left',
+            hidden: false,
+            title: 'Rios',
+            //image: 'images/woman1.png',
+            //caption: 'Source image caption',
+            //website: '<a href="https://www.native-land.ca" target="_blank">Native Land website</a>',
+           // author: 'Rudo Kemper, Digital Democracy',
+            //legend: '<span style="font-size: 0.85em;"><em>Indigenous community names on the map courtesy of Native Land Digital</em></span>',
+            description: 'The Sierra Nevada de Santa Maria is home to more than 50.000 inhabitants among four indigenous communities, direct descendants of the ancient Tayrona culture. This landscapes is a place of worship - no less worthy of veneration than a temple, church, or mosque. As guardians of the earth, they believe it is their duty to protect natural resources and show an ecological alternative to industrialized societies. To the indigenous communities of this Sierra, this landscape is the literal, beating heart of the world.',
+            location: {
+                center: [-73.57919, 10.24514],
+                zoom: 8.59,
+                pitch: 34.06,
+                bearing: 34.44,
+                speed: 0.2
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            spinGlobe: false,
+            mapInteractive: false,
+            callback: '',
+            onChapterEnter: [
+                
+                    {
+                        layer: 'rios',
+                       opacity: 1,
+                       
+                     }
+                 
+            ],
+            onChapterExit: [
+                
+                    {
+                        layer: 'rios',
+                       opacity: 0
+                     }
+                
+            ]
+        }, 
+        {
+            id: 'linea-negra-provectada',
+            alignment: 'left',
+            hidden: false,
+            title: 'Linea Negra Provectada',
+            //image: 'images/woman1.png',
+            //caption: 'Source image caption',
+            //website: '<a href="https://www.native-land.ca" target="_blank">Native Land website</a>',
+           // author: 'Rudo Kemper, Digital Democracy',
+            //legend: '<span style="font-size: 0.85em;"><em>Indigenous community names on the map courtesy of Native Land Digital</em></span>',
+            description: 'The Sierra Nevada de Santa Maria is home to more than 50.000 inhabitants among four indigenous communities, direct descendants of the ancient Tayrona culture. This landscapes is a place of worship - no less worthy of veneration than a temple, church, or mosque. As guardians of the earth, they believe it is their duty to protect natural resources and show an ecological alternative to industrialized societies. To the indigenous communities of this Sierra, this landscape is the literal, beating heart of the world.',
+            location: {
+                center: [-73.83273, 11.16352],
+                zoom: 8.55,
+                pitch: 54,
+                bearing: 138.40,
+                speed: 0.7
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            spinGlobe: false,
+            mapInteractive: false,
+            callback: '',
+            onChapterEnter: [
+                
+                    {
+                        layer: 'linea-negra-provectada',
+                       opacity: 1,
+                       
+                     }
+                 
+            ],
+            onChapterExit: [
+                
+                    {
+                        layer: 'linea-negra-provectada',
+                       opacity: 0
+                     }
+                
+            ]
+        }, 
+        {
+            id: 'parques-naturales',
+            alignment: 'left',
+            hidden: false,
+            title: 'Parques Naturales',
+            //image: 'images/woman1.png',
+            //caption: 'Source image caption',
+            //website: '<a href="https://www.native-land.ca" target="_blank">Native Land website</a>',
+           // author: 'Rudo Kemper, Digital Democracy',
+            //legend: '<span style="font-size: 0.85em;"><em>Indigenous community names on the map courtesy of Native Land Digital</em></span>',
+            description: 'The Sierra Nevada de Santa Maria is home to more than 50.000 inhabitants among four indigenous communities, direct descendants of the ancient Tayrona culture. This landscapes is a place of worship - no less worthy of veneration than a temple, church, or mosque. As guardians of the earth, they believe it is their duty to protect natural resources and show an ecological alternative to industrialized societies. To the indigenous communities of this Sierra, this landscape is the literal, beating heart of the world.',
+            location: {
+                center: [-73.79043, 10.98062],
+                zoom: 8.8,
+                pitch: 0,
+                bearing: 0,
+                speed: 0.2
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            spinGlobe: false,
+            mapInteractive: false,
+            callback: '',
+            onChapterEnter: [
+                
+                    {
+                        layer: 'parques-naturales',
+                       opacity: 1,
+                       
+                     }
+                 
+            ],
+            onChapterExit: [
+                
+                    {
+                        layer: 'parques-naturales',
+                       opacity: 0
+                     }
+                
+            ]
+        },
+        {
             id: 'Mission',
             alignment: 'left',
             hidden: false,
@@ -145,7 +329,7 @@ var config = {
            location: {
                 center: [-73.29813,  9.94819],
                 zoom: 8.92,
-                pitch: 75.00,
+                pitch: 35.00,
                 bearing: 0
             },
             mapAnimation: 'flyTo',
