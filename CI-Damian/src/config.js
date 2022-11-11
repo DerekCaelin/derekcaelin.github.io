8a80fa17c56312b3e87879aa684574c672beca59
Derek Caelin <derekcaelin@protonmail.com>
2022-11-11 11:42:06 -0500

    new storymap
    created a second version of the story, uploaded some files, etc etc.

--- CI-Damian/src/config.js
+++ CI-Damian/src/config.js
@@ -21,19 +21,55 @@ var config = {
     footer: 'Source: source citations, etc.<br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
         {
-            id: 'introduction',
-            alignment: 'left',
+            id: 'mangrove-coast',
+            alignment: 'right',
             hidden: false,
-            title: 'Introduction',
+            title: 'Mangroves',
             //image: 'images/overview-map.png',
             //caption: 'Source image caption',
             //website: '<a href="https://www.native-land.ca" target="_blank">Native Land website</a>',
            // author: 'Rudo Kemper, Digital Democracy',
             //legend: '<span style="font-size: 0.85em;"><em>Indigenous community names on the map courtesy of Native Land Digital</em></span>',
-            description: "filler text: if we don't kind of take a step back and understand what else is going on in the landscape, so sometimes even kind of show the boundaries, allow them to get a sense of place, and then remove them and start to talk about what the landscape really is.",
+            description: "The flat land along the coast is made from the sediment washed into the ocean by Amazon that have been pushed up along the coast of South America. That's where the Dutch and the British grew their sugar cane here because of the rich, fertile soil that has emerged from the Amazon. That's why they brought our ancestors to this place, because of that coast there.<br><br><center><iframe width='350' height='200' src='https://www.youtube.com/embed/0dGNTktx-Pk?controls=0' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></center></iframe>",
             location: {
-                center: [-58.82264, 4.09495],
-                zoom: 5.66,
+                center: [-59.21041, 8.09175],
+                zoom: 7.22,
+                pitch: 47.00,
+                bearing: -156.80
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            spinGlobe: false,
+            mapInteractive: true,
+            callback: '',
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+        {
+            id: 'concrete',
+            alignment: 'left',
+            hidden: false,
+            title: 'Concrete',
+            image: 'images/concrete.gif',
+            //caption: 'Source image caption',
+            //website: '<a href="https://www.native-land.ca" target="_blank">Native Land website</a>',
+           // author: 'Rudo Kemper, Digital Democracy',
+            //legend: '<span style="font-size: 0.85em;"><em>Indigenous community names on the map courtesy of Native Land Digital</em></span>',
+            description: "Hard coastal structures such as concrete seawalls cause wave energy to be reflected, creating more turbulence and much higher wave heights along these human-engineered shores, and ultimately making it difficult for mudflats to form and remain.",
+            location: {
+                center: [-58.21107, 6.82913],
+                zoom: 15.97,
                 pitch: 0,
                 bearing: 0
             },
@@ -57,193 +93,78 @@ var config = {
             ]
         },
         {
-            id: 'deforestation-choice',
-            alignment: 'right',
-            hidden: false,
-            title: 'Deforestation is a choice.',
-           // image: 'images/long-banner.png',
-           // caption: 'Photo Credits: "Kanuku Mountains"/"Mangroves"/"Giant Amazon Water Lily"/"Forest and Savannah Grassland" - ©Pete Oxford/iLCP; "Konashen Women" - ©Piotr Naskrecki; "Giant Otter" - ©Conservation International/photo by John Martin; "Scarlet Macaw" © Conservation International/photo by Haroldo Castro',
-           description: "We look at Brzail to see a choice for Guyana. In Brazil, you can kind of see this herringbone pattern of deforestation. For us over here, but it's dark green forest. Which is the path we want to choose?",
-           location: {
-                center: [-59.34351, 1.45006],
-                zoom: 6.49,
-                pitch: 0,
-                bearing: -50
-            },
-            mapAnimation: 'flyTo',
-            rotateAnimation: false,
-            mapInteractive: false,
-            callback: '',
-            onChapterEnter: [],
-            onChapterExit: []
-        },
-        {
-            id: 'bartica',
-            alignment: 'right',
-            hidden: false,
-            title: 'Bartica',
-            //image: 'images/long-banner.png',
-            //caption: 'Photo Credits: "Kanuku Mountains"/"Mangroves"/"Giant Amazon Water Lily"/"Forest and Savannah Grassland" - ©Pete Oxford/iLCP; "Konashen Women" - ©Piotr Naskrecki; "Giant Otter" - ©Conservation International/photo by John Martin; "Scarlet Macaw" © Conservation International/photo by Haroldo Castro',
-            description: "Most people in Ghana don't really know about the story of this river, but they know about a small town here called Bartica. This is where that river finally hits. It's all the tributaries come in from the Guyana side of the story.",
-            location: {
-                center: [-58.65802, 6.37643],
-                zoom: 12.06,
-                pitch: 41.50,
-                bearing: -96.76
-            },
-            mapAnimation: 'flyTo',
-            rotateAnimation: false,
-            mapInteractive: false,
-            callback: '',
-            onChapterEnter: [],
-            onChapterExit: []
-        },
-        {
-            id: 'pollution',
+            id: 'Georgetown',
             alignment: 'left',
             hidden: false,
-            title: 'Mining',
-            //image: 'images/immaculata-casimero.png',
-            //caption: 'Photo Credits: "Kanuku Mountains"/"Mangroves"/"Giant Amazon Water Lily"/"Forest and Savannah Grassland" - ©Pete Oxford/iLCP; "Konashen Women" - ©Piotr Naskrecki; "Giant Otter" - ©Conservation International/photo by John Martin; "Scarlet Macaw" © Conservation International/photo by Haroldo Castro',
-            description: "But as you can see, the color change happened a long time before it hit. So like, you have this right on the border, this again on the border. But both kind of signal in a warning as this is in your future if you don't get it right. Conversely, you have the opportunity to apply this by making different choices. So like the deforestation agriculture story in Brazil, and the large scale kind of irresponsible mining in the Venezuelan part of this story.",
+            title: 'Georgetown',
+            //image: 'images/overview-map.png',
+            //caption: 'Source image caption',
+            //website: '<a href="https://www.native-land.ca" target="_blank">Native Land website</a>',
+           // author: 'Rudo Kemper, Digital Democracy',
+            //legend: '<span style="font-size: 0.85em;"><em>Indigenous community names on the map courtesy of Native Land Digital</em></span>',
+            description: "Georgetown is particularly beset by flooding due to the engineering decisions that we continue to make about our coastline<br><br></center><iframe width='350' height='200' src='https://www.youtube.com/embed/yuWAun1mJjM?controls=0' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay = 'true'; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen>",
             location: {
-                center: [-61.77681, 6.42177],
-                zoom: 13.71,
-                pitch: 39.53,
-                bearing: 26.29
+                center: [-58.14961, 6.80326],
+                zoom: 11.98,
+                pitch: 0,
+                bearing: 0
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
-            mapInteractive: false,
+            spinGlobe: false,
+            mapInteractive: true,
             callback: '',
-            onChapterEnter: [],
-            onChapterExit: []
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
         },
         {
-            id: 'brazil',
+            id: 'danzig-1',
             alignment: 'left',
             hidden: false,
-            title: 'Savanah and fragmented forest',
-            //image: 'images/immaculata-casimero.png',
-            //caption: 'Photo Credits: "Kanuku Mountains"/"Mangroves"/"Giant Amazon Water Lily"/"Forest and Savannah Grassland" - ©Pete Oxford/iLCP; "Konashen Women" - ©Piotr Naskrecki; "Giant Otter" - ©Conservation International/photo by John Martin; "Scarlet Macaw" © Conservation International/photo by Haroldo Castro',
-            description: "In Brazil, they moving from the savanna and kind of breaking, fragmenting forests, creating these open spaces. So the forest gaps in between are shrinking, because every year there's a fire. It just eats away at the edges, and eventually, the green disappears and the system flips.",
+            title: 'Danzig Collapse',
+            //image: 'images/overview-map.png',
+            //caption: 'Source image caption',
+            //website: '<a href="https://www.native-land.ca" target="_blank">Native Land website</a>',
+           // author: 'Rudo Kemper, Digital Democracy',
+            //legend: '<span style="font-size: 0.85em;"><em>Indigenous community names on the map courtesy of Native Land Digital</em></span>',
+            description: "At Danzig Beach, you can see this process in action as the concrete dropped and the sea flodded in.<br><br></center><iframe width='350' height='200' src='https://www.youtube.com/embed/ojVULdJRiAs?controls=0' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay = 'true'; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen>",
             location: {
-                center: [-61.24512,  2.78822],
-                zoom: 11.08,
+                center: [-57.86458, 6.65785],
+                zoom: 15.05,
                 pitch: 0,
                 bearing: 0
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
-            mapInteractive: false,
+            spinGlobe: false,
+            mapInteractive: true,
             callback: '',
-            onChapterEnter: [],
-            onChapterExit: []
-        },
-        {
-            id: 'brazil-2',
-            alignment: 'left',
-            hidden: false,
-            //title: 'Savanah and fragmented forest',
-            //image: 'images/immaculata-casimero.png',
-            //caption: 'Photo Credits: "Kanuku Mountains"/"Mangroves"/"Giant Amazon Water Lily"/"Forest and Savannah Grassland" - ©Pete Oxford/iLCP; "Konashen Women" - ©Piotr Naskrecki; "Giant Otter" - ©Conservation International/photo by John Martin; "Scarlet Macaw" © Conservation International/photo by Haroldo Castro',
-            description: "It's looking much less like a forested landscape than, say, a savanna landscape.",
-            location: {
-                center: [-60.99273,  2.73031],
-                zoom: 10.10,
-                pitch: 0,
-                bearing: 0
-            },
-            mapAnimation: 'flyTo',
-            rotateAnimation: false,
-            mapInteractive: false,
-            callback: '',
-            onChapterEnter: [],
-            onChapterExit: []
-        },
-        {
-            id: 'rice',
-            alignment: 'right',
-            hidden: false,
-            title: 'Rice',
-            //image: 'images/immaculata-casimero.png',
-            //caption: 'Photo Credits: "Kanuku Mountains"/"Mangroves"/"Giant Amazon Water Lily"/"Forest and Savannah Grassland" - ©Pete Oxford/iLCP; "Konashen Women" - ©Piotr Naskrecki; "Giant Otter" - ©Conservation International/photo by John Martin; "Scarlet Macaw" © Conservation International/photo by Haroldo Castro',
-            description: "So, this let me show you the Brazil side. So you see this is This is rice happening on the Brazilian side, and this is the Guyana side. So this is this is what natural savannas look like tropical savannas. And this is Brazil trying to cultivate them with with like fertilizers and stuff. Now they're able to do it only on these areas, these low areas closer to the river is on a kind of a slope downwards from this space.",
-            location: {
-                center: [-59.53744,  3.600449],
-                zoom: 10.09,
-                pitch: 0,
-                bearing: 0
-            },
-            mapAnimation: 'flyTo',
-            rotateAnimation: false,
-            mapInteractive: false,
-            callback: '',
-            onChapterEnter: [],
-            onChapterExit: []
-        },
-        {
-            id: 'rice-2',
-            alignment: 'right',
-            hidden: false,
-            //title: 'Rice 2',
-            //image: 'images/immaculata-casimero.png',
-            //caption: 'Photo Credits: "Kanuku Mountains"/"Mangroves"/"Giant Amazon Water Lily"/"Forest and Savannah Grassland" - ©Pete Oxford/iLCP; "Konashen Women" - ©Piotr Naskrecki; "Giant Otter" - ©Conservation International/photo by John Martin; "Scarlet Macaw" © Conservation International/photo by Haroldo Castro',
-            description: "In Guyana, we always looked at Brazil and so that they could grow rice in there and Sivanna that we should do. But there's one problem, like it doesn't make sense unless it's heavily subsidized by the agricultural industry.",
-            location: {
-                center: [-59.68925, 3.70693],
-                zoom: 13.39,
-                pitch: 0,
-                bearing: 0
-            },
-            mapAnimation: 'flyTo',
-            rotateAnimation: false,
-            mapInteractive: false,
-            callback: '',
-            onChapterEnter: [],
-            onChapterExit: []
-        },
-        {
-            id: 'rice-3',
-            alignment: 'right',
-            hidden: false,
-            //title: 'Rice 2',
-            //image: 'images/immaculata-casimero.png',
-            //caption: 'Photo Credits: "Kanuku Mountains"/"Mangroves"/"Giant Amazon Water Lily"/"Forest and Savannah Grassland" - ©Pete Oxford/iLCP; "Konashen Women" - ©Piotr Naskrecki; "Giant Otter" - ©Conservation International/photo by John Martin; "Scarlet Macaw" © Conservation International/photo by Haroldo Castro',
-            description: "On Ghana side of things, ut is natural tropical savanah. Here we see the watersheds come quite close to each other, and flow in different directions. That is occurrs all over this landscape. So water flows in one part to Brazil to the left, and right to Ghana.",
-            location: {
-                center: [-59.42566, 3.71049],
-                zoom: 11.09,
-                pitch: 0,
-                bearing: 0
-            },
-            mapAnimation: 'flyTo',
-            rotateAnimation: false,
-            mapInteractive: false,
-            callback: '',
-            onChapterEnter: [],
-            onChapterExit: []
-        },
-        {
-            id: 'rice 4',
-            alignment: 'right',
-            hidden: false,
-            //title: 'Caroline Jacbos',
-            //image: 'images/immaculata-casimero.png',
-            //caption: 'Photo Credits: "Kanuku Mountains"/"Mangroves"/"Giant Amazon Water Lily"/"Forest and Savannah Grassland" - ©Pete Oxford/iLCP; "Konashen Women" - ©Piotr Naskrecki; "Giant Otter" - ©Conservation International/photo by John Martin; "Scarlet Macaw" © Conservation International/photo by Haroldo Castro',
-            description: "So trying to grow rice, the way Brazil is doing it in this space is kind of a biological bomb. So we look across we see Brazil is doing it, but we fail to recognize this floods is slow. And if we put any fertilizer, it's just going to liquefy onto the landscape and cause more problems.",
-            location: {
-                center: [-59.51788,  3.68975],
-                zoom: 10.76,
-                pitch: 0,
-                bearing: 0
-            },
-            mapAnimation: 'flyTo',
-            rotateAnimation: false,
-            mapInteractive: false,
-            callback: '',
-            onChapterEnter: [],
-            onChapterExit: []
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
         }
+        
+        
     ]
 };
