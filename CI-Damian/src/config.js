b1e81f8061755d18f9e9d388b23ef2837d5f6249
Derek Caelin <derekcaelin@protonmail.com>
2022-11-11 14:23:06 -0500

    more updates
    New chapters, new locations, new media

--- CI-Damian/src/config.js
+++ CI-Damian/src/config.js
@@ -32,8 +32,8 @@ var config = {
             //legend: '<span style="font-size: 0.85em;"><em>Indigenous community names on the map courtesy of Native Land Digital</em></span>',
             description: "Guyana lies in a strange area just north of the Amazon river. Most of our rivers aren't connected to the Amazon; instead, they flow north into the Atlantic. We're the only English speaking country in South America. To the east of us is what was formerly Dutch Guyana, Suriname, and then the east of Suriname is French, Ghana and then Venezuela to the west and Brazil to the south. So all of the there about four languages four distinct cultures be in this small space.",
             location: {
-                center: [-57.75175, 4.25187],
-                zoom: 5.38,
+                center: [-58, 4.25187],
+                zoom: 6,
                 pitch: 0,
                 bearing: 0
             },
@@ -129,6 +129,151 @@ var config = {
             ]
         },
         {
+            id: 'mud1',
+            alignment: 'right',
+            hidden: false,
+            //title: 'Seawalls',
+            image: 'images/restoredforest.jpg',
+            caption: 'Restored black mangrove forest, Guyana. Image credit, NAREI',
+            //website: '<a href="https://www.native-land.ca" target="_blank">Native Land website</a>',
+            //author: 'NAREI',
+            //legend: '<span style="font-size: 0.85em;"><em>Indigenous community names on the map courtesy of Native Land Digital</em></span>',
+            description: "Imagine yourself relaxing somewhere along the 280 miles of Guyana’s seawalls. Many of us choose to face inwards, away from the muddy water that, at first glance, seems dirty. But this water holds a fascinating secret, one that has shaped our past and may now hold the key to a prosperous future on this coast.",
+            location: {
+                center: [-57.99961, 6.78160],
+                zoom: 16.13,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'easeTo',
+            rotateAnimation: false,
+            spinGlobe: false,
+            mapInteractive: true,
+            callback: '',
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+        {
+            id: 'mud2',
+            alignment: 'center',
+            hidden: false,
+            //title: 'Seawalls',
+            image: 'images/mudbanks_move.gif',
+            //caption: 'Restored black mangrove forest, Guyana. Image credit, NAREI',
+            //website: '<a href="https://www.native-land.ca" target="_blank">Native Land website</a>',
+            //author: 'NAREI',
+            //legend: '<span style="font-size: 0.85em;"><em>Indigenous community names on the map courtesy of Native Land Digital</em></span>',
+            description: "The flat land along the coast is made from the sediment washed into the ocean by Amazon that have been pushed up along the coast of South America. That's where the Dutch and the British grew their sugar cane here because of the rich, fertile soil that has emerged from the Amazon. That's why they brought our ancestors to this place, because of that coast there. ",
+            location: {
+                center: [-55.82815, 2.63535],
+                zoom: 6,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'easeTo',
+            rotateAnimation: false,
+            spinGlobe: false,
+            mapInteractive: true,
+            callback: '',
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+        
+        {
+            id: 'mangrove-coast',
+            alignment: 'right',
+            hidden: false,
+            title: 'Mangroves',
+            //image: 'images/overview-map.png',
+            //caption: 'Source image caption',
+            //website: '<a href="https://www.native-land.ca" target="_blank">Native Land website</a>',
+           // author: 'Rudo Kemper, Digital Democracy',
+            //legend: '<span style="font-size: 0.85em;"><em>Indigenous community names on the map courtesy of Native Land Digital</em></span>',
+            description: "Mangroves and other salt-loving vegetation play a vital role in retaining this sediment and building out soil. They grow out onto these exposed mudflats, expanding farther into the ocean. This is how Guyana’s Low Coastal Plain was formed and why Guyana’s most fertile soils are found here.<br><br><center><iframe width='100%' height='200' src='https://www.youtube.com/embed/0dGNTktx-Pk?controls=0' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></center></iframe>",
+            location: {
+                center: [-60.11765, 8.60239],
+                zoom: 16.11,
+                pitch: 0,
+                bearing: -168.60
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            spinGlobe: false,
+            mapInteractive: true,
+            callback: '',
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+        {
+            id: 'colonial-legacy',
+            alignment: 'right',
+            hidden: false,
+            title: 'Colonial Legacy',
+            //image: 'images/overview-map.png',
+            //caption: 'Source image caption',
+            //website: '<a href="https://www.native-land.ca" target="_blank">Native Land website</a>',
+           // author: 'Rudo Kemper, Digital Democracy',
+            //legend: '<span style="font-size: 0.85em;"><em>Indigenous community names on the map courtesy of Native Land Digital</em></span>',
+            description: "In Guyana, however, our unique colonial history resulted in the large-scale construction of sea walls and dams directly along the coast, forever changing the behaviour of coastal currents and interrupting this natural earth engine.",
+            location: {
+                center: [-58.51004, 7.35951],
+                zoom: 14.15,
+                pitch: 0,
+                bearing: -168.60
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            spinGlobe: false,
+            mapInteractive: true,
+            callback: '',
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+        {
             id: 'concrete',
             alignment: 'left',
             hidden: false,
@@ -165,52 +310,16 @@ var config = {
             ]
         },
         {
-            id: 'mangrove-coast',
-            alignment: 'right',
-            hidden: false,
-            title: 'Mangroves',
-            //image: 'images/overview-map.png',
-            //caption: 'Source image caption',
-            //website: '<a href="https://www.native-land.ca" target="_blank">Native Land website</a>',
-           // author: 'Rudo Kemper, Digital Democracy',
-            //legend: '<span style="font-size: 0.85em;"><em>Indigenous community names on the map courtesy of Native Land Digital</em></span>',
-            description: "The flat land along the coast is made from the sediment washed into the ocean by Amazon that have been pushed up along the coast of South America. That's where the Dutch and the British grew their sugar cane here because of the rich, fertile soil that has emerged from the Amazon. That's why they brought our ancestors to this place, because of that coast there.<br><br><center><iframe width='350' height='200' src='https://www.youtube.com/embed/0dGNTktx-Pk?controls=0' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></center></iframe>",
-            location: {
-                center: [-59.21041, 8.09175],
-                zoom: 7.22,
-                pitch: 47.00,
-                bearing: -156.80
-            },
-            mapAnimation: 'flyTo',
-            rotateAnimation: false,
-            spinGlobe: false,
-            mapInteractive: true,
-            callback: '',
-            onChapterEnter: [
-                // {
-                //     layer: 'layer-name',
-                //     opacity: 1,
-                //     duration: 5000
-                // }
-            ],
-            onChapterExit: [
-                // {
-                //     layer: 'layer-name',
-                //     opacity: 0
-                // }
-            ]
-        },
-        {
-            id: 'concrete',
+            id: 'modern-troubles',
             alignment: 'left',
             hidden: false,
-            title: 'Concrete',
-            image: 'images/concrete.gif',
+            //title: 'Concrete',
+            //image: 'images/concrete.gif',
             //caption: 'Source image caption',
             //website: '<a href="https://www.native-land.ca" target="_blank">Native Land website</a>',
            // author: 'Rudo Kemper, Digital Democracy',
             //legend: '<span style="font-size: 0.85em;"><em>Indigenous community names on the map courtesy of Native Land Digital</em></span>',
-            description: "Hard coastal structures such as concrete seawalls cause wave energy to be reflected, creating more turbulence and much higher wave heights along these human-engineered shores, and ultimately making it difficult for mudflats to form and remain.",
+            description: "These same locations are where mangroves are missing today, and erosion troubles the coast. ",
             location: {
                 center: [-58.21107, 6.82913],
                 zoom: 15.97,
@@ -246,7 +355,7 @@ var config = {
             //website: '<a href="https://www.native-land.ca" target="_blank">Native Land website</a>',
            // author: 'Rudo Kemper, Digital Democracy',
             //legend: '<span style="font-size: 0.85em;"><em>Indigenous community names on the map courtesy of Native Land Digital</em></span>',
-            description: "Georgetown is particularly beset by flooding due to the engineering decisions that we continue to make about our coastline<br><br></center><iframe width='350' height='200' src='https://www.youtube.com/embed/yuWAun1mJjM?controls=0' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay = 'true'; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen>",
+            description: "Georgetown is particularly beset by flooding due to the engineering decisions that we continue to make about our coastline<br><br></center><iframe width='100%' height='200' src='https://www.youtube.com/embed/yuWAun1mJjM?controls=0' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay = 'true'; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen>",
             location: {
                 center: [-58.14961, 6.80326],
                 zoom: 11.98,
@@ -282,7 +391,7 @@ var config = {
             //website: '<a href="https://www.native-land.ca" target="_blank">Native Land website</a>',
            // author: 'Rudo Kemper, Digital Democracy',
             //legend: '<span style="font-size: 0.85em;"><em>Indigenous community names on the map courtesy of Native Land Digital</em></span>',
-            description: "At Danzig Beach, you can see this process in action as the concrete dropped and the sea flodded in.<br><br></center><iframe width='350' height='200' src='https://www.youtube.com/embed/ojVULdJRiAs?controls=0' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay = 'true'; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen>",
+            description: "At Danzig Beach, you can see this process in action as the concrete dropped and the sea flodded in.<br><br></center><iframe width='100%' height='200' src='https://www.youtube.com/embed/ojVULdJRiAs?controls=0' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay = 'true'; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen>",
             location: {
                 center: [-57.86458, 6.65785],
                 zoom: 15.05,
